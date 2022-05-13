f45568682335c5473f33aa9737da9361639b6bdc
ABDÜLKERİM SEKİLİ <19030411042@ybu.edu.tr>
2022-05-13 23:40:10 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer{DateOfBirth = new DateTime(1998,2,26),FirstName = "Yunus Emre",LastName = "Ulusan",NationalityId = "11512086116"});
+        }
+    }
+}
